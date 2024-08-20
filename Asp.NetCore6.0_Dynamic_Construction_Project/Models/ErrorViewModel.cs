2a3dae32b8e098b2b15a22a5bac95b05657091ec
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-20 18:13:18 +0300

    Add project files.

---
+++ Asp.NetCore6.0_Dynamic_Construction_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore6._0_Dynamic_Construction_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
