d74c6033fd76df5c081630e5e7008a53b8910b4d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-20 20:40:21 +0300

    Project References Added

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("server=.;database=ConstructionDBV1.0;integrated security=true;");
+            optionsBuilder.UseSqlServer("server=45.10.150.55\\MSSQLSERVER2012;database=menduhDB;user=sqladmin;password=Yunus6565*");
+
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SpecialProduct> SpecialProducts { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Video> Videos { get; set; }
+    }
+}
