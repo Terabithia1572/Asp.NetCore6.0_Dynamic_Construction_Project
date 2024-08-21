4b9ebea3de06243660ba5dc3c8b835fbb4ab1fe9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-22 00:50:25 +0300

    The Product section was made dynamic and data was pulled from the database

--- Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/Layout/_CommentViewComponentPartial.cs
+++ Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/Layout/_CommentViewComponentPartial.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_Dynamic_Construction_Project.ViewComponents.Layout
 {
     public class _CommentViewComponentPartial:ViewComponent
     {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = commentManager.GetList();
+            return View(values);
         }
     }
 }
