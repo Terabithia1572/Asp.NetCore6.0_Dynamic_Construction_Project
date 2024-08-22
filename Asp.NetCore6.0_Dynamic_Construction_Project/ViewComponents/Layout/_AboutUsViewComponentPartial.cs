84ceab39fb61f0ed6c655a80169ba9d09590b365
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-23 00:38:56 +0300

    Comment Page Added + Problems Fixed

--- Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/Layout/_AboutUsViewComponentPartial.cs
+++ Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/Layout/_AboutUsViewComponentPartial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_Dynamic_Construction_Project.ViewComponents.Layout
 {
     public class _AboutUsViewComponentPartial:ViewComponent
     {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = commentManager.GetList();
+            return View(values);
         }
     }
 }
---
+++ DataAccessLayer/Migrations/20240822211505_mig_Comment2_removed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_Comment2_removed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments2",
+                columns: table => new
+                {
+                    Comment2ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Comment2Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comment2Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Comment2Status = table.Column<bool>(type: "bit", nullable: false),
+                    Comment2Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comment2UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image2Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments2", x => x.Comment2ID);
+                });
+        }
+    }
+}
