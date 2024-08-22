d636d0360ae6e70762a9794d6698f81bc343a48d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-22 23:23:06 +0300

    Gallery Process Added

--- Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/Layout/_GalleryViewComponentPartial.cs
+++ Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/Layout/_GalleryViewComponentPartial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_Dynamic_Construction_Project.ViewComponents.Layout
 {
     public class _GalleryViewComponentPartial:ViewComponent
     {
+        ImageManager imageManager = new ImageManager(new EfImageRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = imageManager.GetList();
+            return View(values);
         }
     }
 }
