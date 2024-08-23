9f2fd279d1879cd1156d4fd043f38f4072165f53
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-23 19:32:04 +0300

    Video Page Added + Video Process Added

---
+++ Asp.NetCore6.0_Dynamic_Construction_Project/Controllers/VideoController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_Dynamic_Construction_Project.Controllers
+{
+    public class VideoController : Controller
+    {
+        VideoManager videoManager = new VideoManager(new EfVideoRepository());
+        public IActionResult Index()
+        {
+            var values = videoManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult UpdateVideo(int id)
+        {
+            var values = videoManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateVideo(Video video)
+        {
+            video.VideoStatus = true;
+            videoManager.TUpdate(video);
+
+            return RedirectToAction("Index", "Video");
+        }
+    }
+}
