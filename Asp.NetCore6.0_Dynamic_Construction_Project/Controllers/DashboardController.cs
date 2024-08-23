0adf4c9363d7763284c917288b61ca514d25cb9c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-23 18:57:08 +0300

    Dashboard + Footer Updated

--- Asp.NetCore6.0_Dynamic_Construction_Project/Controllers/DashboardController.cs
+++ Asp.NetCore6.0_Dynamic_Construction_Project/Controllers/DashboardController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_Dynamic_Construction_Project.Controllers
 {
     public class DashboardController : Controller
     {
+        ProductManager productManager = new ProductManager(new EfProductRepository());
+        ImageManager imageManager = new ImageManager(new EfImageRepository());
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+        AdminManager adminManager = new AdminManager(new EfAdminRepository());
+        Context context = new Context();
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult CardPage()
+        {
+            var toplamurunSayisi = productManager.GetList().Count();
+            ViewBag.ToplamUrunSayisi = toplamurunSayisi;
+            var toplamresimSayisi = imageManager.GetList().Count();
+            ViewBag.ToplamResimSayisi = toplamresimSayisi;
+            var toplamYorumSayisi = commentManager.GetList().Count();
+            ViewBag.ToplamYorumSayisi = toplamYorumSayisi;
             return View();
         }
     }
