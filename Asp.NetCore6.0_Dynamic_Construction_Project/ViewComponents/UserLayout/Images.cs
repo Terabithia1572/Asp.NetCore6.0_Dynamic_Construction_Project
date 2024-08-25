d2578331bfd0cb64557207b12fcc5e2702001d7b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-26 01:40:30 +0300

    Images Page Updated

--- Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/UserLayout/Images.cs
+++ Asp.NetCore6.0_Dynamic_Construction_Project/ViewComponents/UserLayout/Images.cs
@@ -9,7 +9,10 @@ namespace Asp.NetCore6._0_Dynamic_Construction_Project.ViewComponents.UserLayout
         ImageManager imageManager = new ImageManager(new EfImageRepository());
         public IViewComponentResult Invoke(int page = 1)
         {
-            var values = imageManager.GetList();
+            var values = imageManager.GetList()
+                          .OrderByDescending(x => x.ImageID) // Listeyi Id'ye göre tersten sırala (en son eklenen en üstte olacak)
+                          .Take(8)                      // Son 8 öğeyi al
+                          .ToList();
             return View(values);
         }
     }
