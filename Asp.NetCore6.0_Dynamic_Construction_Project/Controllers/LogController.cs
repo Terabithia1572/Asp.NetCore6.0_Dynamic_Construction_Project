9adadc7579f826a8229dd2eac05494f6704fa5c0
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-27 20:46:02 +0300

    logging added + Products page updated

---
+++ Asp.NetCore6.0_Dynamic_Construction_Project/Controllers/LogController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_Dynamic_Construction_Project.Controllers
+{
+    public class LogController : Controller
+    {
+        LogManager logManager = new LogManager(new EfLogRepository());
+        public IActionResult LogList()
+        {
+            var values = logManager.GetList()
+                         .OrderByDescending(x => x.Id) // Listeyi Id'ye göre tersten sırala (en son eklenen en üstte olacak)
+                         .Take(10)                      // Son 10 öğeyi al
+                         .ToList();
+            return View(values);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/LogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class LogManager : ILogService
+    {
+        ILogDal _logDal;
+
+        public LogManager(ILogDal logDal)
+        {
+            _logDal = logDal;
+        }
+
+        public List<Log> GetList()
+        {
+            return _logDal.GetListAll();
+        }
+
+        public void TAdd(Log t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Log t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Log TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Log t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
